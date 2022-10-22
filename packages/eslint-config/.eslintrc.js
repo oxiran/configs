e49b27da7e86b53cb6c1d3cb4af7b3768fb9f456
廖浩然 <413391153@qq.com>
2022-10-23 00:10:43 +0800

    feat: :sparkles: Add the eslint-config

---
+++ packages/eslint-config/.eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    mocha: true,
+  },
+  plugins: [
+    'eslint-comments',
+  ],
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    /* plugins: import */
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+
+    /* eslint */
+    'no-bitwise': 'off',
+    'func-names': ['error', 'never', { generators: 'as-needed' }],
+    'no-param-reassign': ['error', { props: false }],
+    'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
+    'max-len': [
+      'error',
+      {
+        code: 200,
+        comments: 300,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'no-restricted-syntax': ['error', 'WithStatement'],
+    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    radix: ['error', 'as-needed'],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 1, maxEOF: 1 }],
+    'no-labels': ['error', { allowLoop: true }],
+    'prefer-destructuring': 'off',
+  },
+};
