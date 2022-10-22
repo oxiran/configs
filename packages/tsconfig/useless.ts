fe4e4f94bee39cfcd9b6ff83d38c19bc5ac41ba1
廖浩然 <413391153@qq.com>
2022-10-23 00:24:32 +0800

    feat: :sparkles: Add the ts-config

---
+++ packages/tsconfig/useless.ts
@@ -0,0 +1,6 @@
+// Make this file in order to deal with the error of VSCode.
+// In addition, it has no other effect.
+/**
+ * VSCode Error
+ * 在配置文件“configs/packages/tsconfig/tsconfig.json”中找不到任何输入。
+ */
